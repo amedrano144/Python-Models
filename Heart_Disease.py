0286f52889badd091a86889f378068432373b7c7
Anthony Medrano <74431793+amedrano144@users.noreply.github.com>
2020-12-05 23:35:30 -0800

    Add files via upload

--- Heart_Disease.py
+++ Heart_Disease.py
@@ -1,73 +1,70 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov 28 21:36:18 2020
-
 @author: anthony
 """
 
-'''
-Things to solve:
-    - Use all data
-    - Identify dependent variable (binary?)
-    - Identify relevant independent variables
-    - Apply machine learning algorithms
-    - Compare accuracy of ML algorithms
-'''
-
-
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
 
+# initialize dataframe
 column = ['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal','num']
-
-
 df = pd.DataFrame([],columns = column)
-
 indices = [2,3,8,9,11,15,18,31,37,39,40,43,50,57]
 
 
-
-with open("heart-disease.names") as col_names:
-    colum = col_names.read()
-    
-print(colum)
-
-
-# FOR HUNGARIAN, SWITZERLAND, L0NG-BEACH-VA
+# for HUNGARIAN, SWITZERLAND, L0NG-BEACH-VA datasets
 with open("hungarian.data") as myfile:
     data = myfile.readlines()
-    #print(data)
-    #print(len(data))
 temp_list = []
 temp_list2 = []
 
-
+# clean and organize data
 for i in range(len(data)):
     temp = data[i].split()
-    #print(temp)
     if 'name' in temp:
         temp = temp[0:-1]
         for j in range(len(temp)):
             temp_list.append(float(temp[j]))
         for k in indices:
             temp_list2.append(temp_list[k])
-        #print(temp_list)
         aseries = pd.Series(temp_list2, index = column) #, index = df.columns
-        #print(aseries)
         df = df.append(aseries, ignore_index = True)
-        #print(df)
         temp_list = []
         temp_list2 = []
     else:
         for j in range(len(temp)):
             temp_list.append(float(temp[j]))
 
+# select features to keep/drop based on proportion of missing values
+count_missing_values = [0]*df.shape[1]
+for m in range(df.shape[0]):
+    for n in range(df.shape[1]):
+        if df.iloc[m,n] < 0:
+            count_missing_values[n] = count_missing_values[n] + 1
+keep_indices = [1]*df.shape[1]
+for a, b in enumerate(count_missing_values):
+    if b > df.shape[0]/2:
+        keep_indices[a] = 0
+        
+        
+# create new dataframe with select features     
+filtered_df = pd.DataFrame()
+for x in range(df.shape[1]):
+    if keep_indices[x] == 1:
+        name = column[x]
+        filtered_df[name] = df.iloc[:,x]
+
+
+
 '''
-# FOR PROCESSED.CLEVELAND
+# for PROCESSED.CLEVELAND dataset
 with open("processed.cleveland.data") as myfile:
     data = myfile.readlines()
     #print(data)
@@ -93,32 +90,36 @@ for i in range(len(data)):
     temp_list = []
 '''
 
-
-# ASK LUDOVICO
-# 1. convert into array or list?
-# 2. handle negative/missing/? values
-# 3. randomize order of instances
-# 4. split into train and test
-# 5. try modeling one dataset first, Hungarian
-# 6. try logistic or naive bayes, first, then try different statistical models
-# 7. evaluate accuracy
-# 8. combine datasets into one? or create separate models? try both?
-# 9. improve code last
-        
-# feature selection?
-# column name abstraction?
-
-
-
+# convert dataframe to array and split data
 X = df.loc[:, df.columns != 'num'].to_numpy()
 y = df['num'].to_numpy()
-#X_train, X_test, y_train, y_test = train_test_split(df , y, test_size = 0.3, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X , y, test_size = 0.1)
 
-#print(X_train.shape)
-#print(X_test.shape)
+
+# initialize classifier
+#gnb = GaussianNB()
+#gnb = LogisticRegression(max_iter = 20000)
+gnb = SVC()
+
+# train classifier
+model = gnb.fit(X_train, y_train)
+
+# make predictions
+preds = gnb.predict(X_test)
+print(preds)
+
+# evaluate accuracy
+print("accuracy:",accuracy_score(y_test, preds))
+
+
+
 
 
 '''
+# feature selection?
+with open("heart-disease.names") as col_names:
+    colum = col_names.read()
+# column name abstraction?
 plt.figure(figsize = (12,10))
 cor = X_train.corr()
 sns.heatmap(cor, annot = True, cmap = plt.cm.CMRmap_r)
@@ -138,5 +139,3 @@ def correlation(dataset, threshold):
 corr_features = correlation(X_train, 0.7)
 len(set(corr_features))
 '''
-
-    
